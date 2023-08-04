86e3124b139d614002e6077d85fe25f961f3d5c2
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-08-04 19:38:06 +0200

    fixing Mongo db connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/dbconfig");
+
+connectDB();
 
 const port = process.env.PORT || 9000;
 
